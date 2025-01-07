804d300cd730be1060ab771b28bd24bf0887f40a
venkat <devops.bheemireddy@gmail.com>
2025-01-07 21:54:36 +1100

    vault coding changes

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -91,6 +91,11 @@ resource "azurerm_virtual_machine" "main" {
   }
 }
 
+# this is code is added for docker
+locals {
+  component = var.container ? "${var.component}-docker" : var.component
+}
+
 ### Running provisioner outside virtual machine code block for each component
 ### Else it will destroy and re-create the resources when we perform terraform apply
 resource "null_resource" "ansible" {
@@ -112,9 +117,10 @@ resource "null_resource" "ansible" {
     inline = [
       "sudo dnf install python3.12-pip -y",
       "sudo pip3.12 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/VenkatBheemireddy/roboshop-ansible.git roboshop.yml -e appl=${var.component}"
-      # Commented Raghu Code
-      # "ansible-pull -i localhost, -U https://github.com/raghudevopsb82/roboshop-ansible roboshop.yml -e app_name=${var.component} -e ENV=dev"
+      #"ansible-pull -i localhost, -U https://github.com/VenkatBheemireddy/roboshop-ansible.git roboshop.yml -e appl=${local.component} -e vault_token=${var.vault_token}"
+
+      "ansible-pull -i localhost, -U https://github.com/VenkatBheemireddy/roboshop-ansible.git roboshop.yml -e app_name=${local.component} -e ENV=${var.env} -e vault_token=${var.vault_token}"
+
     ]
   }
 }
