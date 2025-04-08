e230606c36dbea0393a466bf032bd27dfdf159b6
venkat <devops.bheemireddy@gmail.com>
2025-04-08 18:24:49 +1000

    Project Roboshop Ansible with Multi environment

--- provider.tf
+++ provider.tf
@@ -8,8 +8,8 @@ provider "azurerm" {
 }
 
 provider "vault" {
-# address = "http://vault-internal.azdevopsv82.online:8200"
-  address = "http://vault.azdevopsv82.online:8200"
+  address = "http://vault-internal.azdevopsv82.online:8200"
+# address = "http://vault.azdevopsv82.online:8200"
   token   = var.token
 }
 
