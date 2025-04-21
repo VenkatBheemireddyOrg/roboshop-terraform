0e368faec8502f424cc2cb5696da42e21726b148
venkat <devops.bheemireddy@gmail.com>
2025-04-21 21:11:50 +1000

    Project Roboshop Ansible with Multi environment

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -8,20 +8,21 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size = "Standard_D2_v2"
 
-    # # The below 3 lines are needed to enable autoscaling
-    # auto_scaling_enabled = true
-    # min_count  = 1
-    # max_count  = 10
-    #
-    # # this line is required to create aks cluster under same project-setup-1 network
-    # vnet_subnet_id = "/subscriptions/9af0e83a-d3ee-4c3c-a244-3274a3457024/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
+    ###  The following 3 lines are needed to enable "Cluster Autoscaling" or "Node Autoscaling"
+    auto_scaling_enabled = true
+    min_count            = 1
+    max_count            = 10
   }
 
   identity {
     type = "SystemAssigned"
   }
+}
+
+  # # this line is required to create aks cluster under same project-setup-1 network
+  # vnet_subnet_id = "/subscriptions/9af0e83a-d3ee-4c3c-a244-3274a3457024/resourceGroups/project-setup-1/providers/Microsoft.Network/virtualNetworks/project-setup-network/subnets/default"
 
   # # this block is required to create aks cluster under same project-setup-1 network
   # aci_connector_linux {
@@ -36,4 +37,4 @@ resource "azurerm_kubernetes_cluster" "main" {
   # }
 
 
-}
+
