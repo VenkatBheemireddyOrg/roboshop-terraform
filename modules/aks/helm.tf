2c154cf0db0c64435b87fc71e834945f50e01a2c
venkat <devops.bheemireddy@gmail.com>
2025-05-16 20:58:56 +1000

    Latest Changes

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -20,7 +20,7 @@ resource "null_resource" "external-secrets" {
   provisioner "local-exec" {
     ###BEG 20250511 added code as part of github-runner workflow
     command = <<EOF
-       kubectl create secret generic vault-token --from-literal=token=${var.vault_token}
+       kubectl create secret generic vault-token --from-literal=token=${var.vault_token} --namespace kube-system
        kubectl apply -f ${path.module}/files/secretstore.yaml
     EOF
     ###END 20250511 added code as part of github-runner workflow
