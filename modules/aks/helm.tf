4d16ebb84278c35ec37e38d3312dd1c87886c77e
venkat <devops.bheemireddy@gmail.com>
2025-06-03 21:05:23 +1000

    Latest Changes

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -73,33 +73,33 @@ EOF
 }
 
 
-# resource "null_resource" "external-dns-secret" {
-#   depends_on = [null_resource.kubeconfig]
-#   provisioner "local-exec" {
-#     command = <<EOT
-# cat <<-EOF > ${path.module}/azure.json
-# {
-#   "tenantId": "${data.azurerm_subscription.current.tenant_id}",
-#   "subscriptionId": "${data.azurerm_subscription.current.subscription_id}",
-#   "resourceGroup": "${data.azurerm_resource_group.main.name}",
-#   "useManagedIdentityExtension": true,
-#   "userAssignedIdentityID": "${azurerm_kubernetes_cluster.main.kubelet_identity[0].client_id}"
-# }
-# EOF
-# kubectl create secret generic azure-config-file --namespace "kube-system" --from-file=${path.module}/azure.json
-# EOT
-#   }
-# }
-#
-#
-# resource "helm_release" "external-dns" {
-#   depends_on = [null_resource.kubeconfig, null_resource.external-dns-secret]
-#   name       = "external-dns"
-#   repository = "https://kubernetes-sigs.github.io/external-dns/"
-#   chart      = "external-dns"
-#   namespace  = "kube-system"
-#   values = [
-#     file("${path.module}/files/external-dns.yaml")
-#   ]
-# }
-#
+resource "null_resource" "external-dns-secret" {
+  depends_on = [null_resource.kubeconfig]
+  provisioner "local-exec" {
+    command = <<EOT
+cat <<-EOF > ${path.module}/azure.json
+{
+  "tenantId": "${data.azurerm_subscription.current.tenant_id}",
+  "subscriptionId": "${data.azurerm_subscription.current.subscription_id}",
+  "resourceGroup": "${data.azurerm_resource_group.main.name}",
+  "useManagedIdentityExtension": true,
+  "userAssignedIdentityID": "${azurerm_kubernetes_cluster.main.kubelet_identity[0].client_id}"
+}
+EOF
+kubectl create secret generic azure-config-file --namespace "kube-system" --from-file=${path.module}/azure.json
+EOT
+  }
+}
+
+
+resource "helm_release" "external-dns" {
+  depends_on = [null_resource.kubeconfig, null_resource.external-dns-secret]
+  name       = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+  values = [
+    file("${path.module}/files/external-dns.yaml")
+  ]
+}
+
