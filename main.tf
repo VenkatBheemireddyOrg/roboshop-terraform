df37acdd309c67cde1025ccbd87e8c4bd5076532
venkat <devops.bheemireddy@gmail.com>
2025-06-04 14:49:35 +1000

    Latest Changes

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ module "aks" {
 }
 
 ### code to create databases - mongodb, mysql, rabbitmq, redis
-module "databases" {
-  for_each = var.databases
-  source = "./modules/vm"
-  component = each.value["name"]
-  vm_size = each.value["vm_size"]
-  env = var.env
-  vault_token  = var.token
-  container = each.value["container"]
-}
+# module "databases" {
+#   for_each = var.databases
+#   source = "./modules/vm"
+#   component = each.value["name"]
+#   vm_size = each.value["vm_size"]
+#   env = var.env
+#   vault_token  = var.token
+#   container = each.value["container"]
+# }
 
 
--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -102,22 +102,8 @@ resource "helm_release" "external-dns" {
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
   namespace  = "kube-system"
-  # values = [
-  #   file("${path.module}/files/external-dns.yaml")
-  # ]
-  provisioner "local-exec" {
-     command = <<EOF
-         kubectl apply -f ${path.module}/files/external-dns.yaml
-     EOF
-  }
+  values = [
+    file("${path.module}/files/external-dns.yaml")
+  ]
 }
 
-# resource "null_resource" "argocd" {
-#   depends_on = [null_resource.kubeconfig]
-#   provisioner "local-exec" {
-#     command = <<EOF
-# kubectl apply -f ${path.module}/files/argocd-ns.yaml
-# kubectl apply -f ${path.module}/files/argocd.yaml -n argocd
-# EOF
-#   }
-# }
