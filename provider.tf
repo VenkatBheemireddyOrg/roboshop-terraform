96cc0c9841c6989a94de0053ebe73f944c20388c
venkat <devops.bheemireddy@gmail.com>
2025-06-08 14:20:15 +1000

    Latest Changes

--- provider.tf
+++ provider.tf
@@ -5,6 +5,11 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.35.0"
     }
+
+    grafana = {
+      source = "grafana/grafana"
+      version = "3.21.0"
+    }
   }
 }
 
@@ -28,4 +33,10 @@ provider "helm" {
 ### this is required for external-dns step-1
 provider "kubernetes" {
     config_path = "~/.kube/config"
+}
+
+
+provider "grafana" {
+  url  = "http://grafana-${var.env}.azdevopsv82.online/"
+  auth = data.vault_generic_secret.k8s.data["grafana_auth"]
 }
