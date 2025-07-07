3d183fcc2da74cf1d51d35cfcf72f6d9e019c533
venkat <devops.bheemireddy@gmail.com>
2025-07-07 12:25:07 +1000

    Latest Changes

--- modules/aks/helm.tf
+++ modules/aks/helm.tf
@@ -29,15 +29,37 @@ EOF
 #kubectl apply -f /opt/vault-token.yml
 
 
-### Argocd namespace creation
-resource "null_resource" "argocd" {
-  depends_on = [null_resource.kubeconfig]
-  provisioner "local-exec" {
-    command = <<EOF
-kubectl apply -f ${path.module}/files/argocd-ns.yaml
-kubectl apply -f ${path.module}/files/argocd.yaml -n argocd
-EOF
+# ### Argocd namespace creation
+# resource "null_resource" "argocd" {
+#   depends_on = [null_resource.kubeconfig]
+#   provisioner "local-exec" {
+#     command = <<EOF
+# kubectl apply -f ${path.module}/files/argocd-ns.yaml
+# kubectl apply -f ${path.module}/files/argocd.yaml -n argocd
+# EOF
+#   }
+# }
+
+
+## ArgoCD Setup
+resource "helm_release" "argocd" {
+  depends_on = [null_resource.kubeconfig, helm_release.external-dns]
+
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  wait             = false
+
+  set {
+    name  = "global.domain"
+    value = "argocd-${var.env}.azdevopsv82.online"
   }
+
+  values = [
+    file("${path.module}/files/argo-helm.yaml")
+  ]
 }
 
 
