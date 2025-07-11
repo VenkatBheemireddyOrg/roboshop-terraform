8b05784b6f6f3e9f45389ebf57945481d5bd5868
venkat <devops.bheemireddy@gmail.com>
2025-07-11 14:46:17 +1000

    Latest Changes

--- variables.tf
+++ variables.tf
@@ -7,14 +7,3 @@ variable "subscription_id" {
   default = "9af0e83a-d3ee-4c3c-a244-3274a3457024"
 }
 
-variable "client_id" {
-  default = "000ea383-57b2-49ea-854f-97269d0856d2"
-}
-
-variable "client_secret" {
-  default = "LDD8Q~jQF~1QexAASbmOUd2jU0HbAfBPWSyH3cDl"
-}
-
-variable "tenant_id" {
-  default = "b439be94-ce35-4928-a4d0-1fa864d7858e"
-}
